b871d10003b40d629c4d88196f50128a26318b88
utsavberi <utsavberi@gmail.com>
2012-06-23 02:54:10 +0530

    ver 1-idcard designer
    save functionality partial
    open functionality none

---
+++ IDCardManagement/IDCardManagement/Form2.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace IDCardManagement
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.ContextMenuStrip = this.contextMenuStrip1;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(101, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(462, 330);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 89);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tmplbl_MouseDown);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripComboBox2,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1360, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(200, 27);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Items.AddRange(new object[] {
+            "8",
+            "10",
+            "12",
+            "14",
+            "16",
+            "18",
+            "24",
+            "32"});
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(75, 27);
+            this.toolStripComboBox2.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox2_SelectedIndexChanged);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackColor = System.Drawing.Color.Black;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton1.Text = "Font Color";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.BackColor = System.Drawing.Color.White;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.ForeColor = System.Drawing.Color.White;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton2.Text = "BackColor";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton3.Text = "Delete";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton4.Text = "Save";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.ClientSize = new System.Drawing.Size(1360, 737);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "IDCard Designer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.Click += new System.EventHandler(this.Form2_Click);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+    }
+}
---
+++ IDCardManagement/IDCardManagement/Form2.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace IDCardManagement
+{
+    public partial class Form2 : Form
+    {
+        public int Y { get; set; }
+
+        public int X { get; set; }
+        private IDCard idcard;
+
+        public Form2(IDCard idcard)
+        {
+            InitializeComponent();
+            ControlMover.Init(pictureBox1);
+            ControlMover.Init(label1);
+            this.idcard = idcard;
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                toolStripComboBox1.Items.Add(font.Name);
+            }
+
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            if (idcard.backgroundImage != null) panel1.BackgroundImage = idcard.backgroundImage;
+            label1.Text = idcard.title;
+            panel1.Size = new Size(idcard.dimensions.Width * 10, idcard.dimensions.Height * 10);
+            foreach (String str in idcard.selectedFields)
+            {
+                ToolStripItem tmp = contextMenuStrip1.Items.Add(str);
+                tmp.Click += tmpToolStripItem_Click;
+            }
+        }
+
+        private void tmpToolStripItem_Click(object sender, EventArgs e)
+        {
+            ToolStripItem clickedItem = (ToolStripItem)sender;
+            Label tmp = new Label();
+            tmp.BackColor = Color.Transparent;
+            tmp.Text = clickedItem.Text;
+            tmp.Left = X;
+            tmp.Top = Y;
+            tmp.AutoSize = true;
+            tmp.MouseDown += tmplbl_MouseDown;
+            ControlMover.Init(tmp);
+            panel1.Controls.Add(tmp);
+        }
+
+        private void tmplbl_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.None;
+            if (Control.ModifierKeys != Keys.Control)
+                foreach (Control ctl in panel1.Controls) { if (ctl is Label) { ((Label)ctl).BorderStyle = BorderStyle.None; } }
+            Label tmp = sender as Label;
+            tmp.BorderStyle = BorderStyle.FixedSingle;
+            toolStripComboBox1.Text = tmp.Font.FontFamily.Name;
+            toolStripComboBox2.Text = ((int)tmp.Font.Size).ToString();
+            toolStripButton1.BackColor = tmp.ForeColor;
+            toolStripButton2.BackColor = tmp.BackColor;
+
+        }
+
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                X = Cursor.Position.X - panel1.Left;
+                Y = Cursor.Position.Y - panel1.Top - 30;
+            }
+
+        }
+
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctl in panel1.Controls) { if (ctl is Label) { ((Label)ctl).BorderStyle = BorderStyle.None; } }
+
+        }
+
+
+
+        private void Form2_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctl in panel1.Controls) { if (ctl is Label) { ((Label)ctl).BorderStyle = BorderStyle.None; } }
+
+        }
+
+
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control ctl in panel1.Controls)
+            {
+                if (ctl is Label) if (((Label)ctl).BorderStyle == BorderStyle.FixedSingle) ((Label)ctl).Font = new Font(toolStripComboBox1.Text, ctl.Font.Size);
+            }
+        }
+
+        private void toolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control ctl in panel1.Controls)
+            {
+                if (ctl is Label)
+                {
+                    if (((Label)ctl).BorderStyle == BorderStyle.FixedSingle)
+                        ((Label)ctl).Font = new Font(ctl.Font.FontFamily.ToString(), Convert.ToInt32(toolStripComboBox2.Text));
+                }
+            }
+
+        }
+
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                foreach (Control ctl in panel1.Controls)
+                {
+                    if (ctl is Label)
+                    {
+                        if (((Label)ctl).BorderStyle == BorderStyle.FixedSingle)
+                            ((Label)ctl).BackColor = colorDialog1.Color;
+                    }
+                }
+
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            foreach (Control ctl in panel1.Controls) { if (ctl is Label) { ((Label)ctl).BorderStyle = BorderStyle.None; } }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctl in panel1.Controls)
+            {
+                if (ctl is Label)
+                {
+                    if (((Label)ctl).BorderStyle == BorderStyle.FixedSingle)
+                        panel1.Controls.Remove(ctl);
+                    ctl.Dispose();
+                    //((Label)ctl).BackColor = colorDialog1.Color;
+                }
+            }
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                foreach (Control ctl in panel1.Controls)
+                {
+                    if (ctl is Label)
+                    {
+                        if (((Label)ctl).BorderStyle == BorderStyle.FixedSingle)
+                            ((Label)ctl).ForeColor = colorDialog1.Color;
+                    }
+                }
+
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                using (XmlWriter wrt = XmlWriter.Create(saveFileDialog1.FileName))
+                {
+                    wrt.WriteStartDocument();
+                    wrt.WriteStartElement("panel");
+                    foreach (Control ctl in this.panel1.Controls)
+                    {
+                        if (ctl is Label)
+                        {
+                            wrt.WriteStartElement("label");
+                            wrt.WriteAttributeString("text", ((Label)ctl).Text);
+                            wrt.WriteAttributeString("top", ((Label)ctl).Top.ToString());
+                            wrt.WriteAttributeString("left", ((Label)ctl).Left.ToString());
+                            wrt.WriteAttributeString("backcolor", ((Label)ctl).BackColor.ToArgb().ToString()); //Color.FromArgb(int);
+                            wrt.WriteAttributeString("forecolor", ((Label)ctl).ForeColor.ToArgb().ToString());
+                            wrt.WriteAttributeString("font", TypeDescriptor.GetConverter(typeof(Font)).ConvertToString(((Label)ctl).Font)); //Font font = (Font)converter.ConvertFromString(fontString);
+                            wrt.WriteEndElement();
+
+                        }
+                        if (ctl is PictureBox)
+                        {
+                            wrt.WriteStartElement("pictureBpx");
+                            wrt.WriteAttributeString("left", ((PictureBox)ctl).Left.ToString());
+                            wrt.WriteAttributeString("top", ((PictureBox)ctl).Top.ToString());
+                            wrt.WriteEndElement();
+                        }
+
+                    }
+
+                    wrt.WriteStartElement("idCard");
+                    wrt.WriteAttributeString("connectionString", idcard.connectionString);
+                    wrt.WriteAttributeString("height", idcard.dimensions.Height.ToString());
+                    wrt.WriteAttributeString("width", idcard.dimensions.Width.ToString());
+                    wrt.WriteAttributeString("tableName",idcard.tableName);
+                    wrt.WriteAttributeString("title",label1.Text);//idcard.title
+                    string imagebase64String;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        idcard.backgroundImage.Save(ms, idcard.backgroundImage.RawFormat);
+                        byte[] imageBytes = ms.ToArray();
+                        imagebase64String = Convert.ToBase64String(imageBytes);
+                    }
+                    //wrt.WriteAttributeString("backgroundImage", imagebase64String);
+                    foreach (string str in idcard.fields)
+                    {
+
+                        wrt.WriteElementString("field",str);
+
+ 
+                    }
+                    foreach (string str in idcard.selectedFields)
+                    {
+
+                        wrt.WriteElementString("selectedField", str);
+
+
+                    }
+                    wrt.WriteEndElement();//idcard
+                    wrt.WriteEndElement();//panel
+                    wrt.WriteEndDocument();
+                }
+
+        }
+
+    }
+}
